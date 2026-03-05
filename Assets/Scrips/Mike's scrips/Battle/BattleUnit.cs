2a35280f1796a534eef0192188c6eee4e8253b36
whyhoa <phanmichael34@gmail.com>
2026-03-05 19:21:18 +0100

    UAA

--- Assets/Scrips/Mike's scrips/Battle/BattleUnit.cs
+++ Assets/Scrips/Mike's scrips/Battle/BattleUnit.cs
@@ -1,29 +1,18 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
-using UnityEngine;
-using UnityEngine.InputSystem.HID;
 using UnityEngine.UI;
 
 public class BattleUnit : MonoBehaviour
 {
-    [SerializeField] MonsterBaseScript _base;
-    [SerializeField] int level;
     [SerializeField] bool isPlayerUnit;
+
     [SerializeField] BattleHud hud;
 
-    public bool IsPlayerUnit
-    {
-        get { return isPlayerUnit; }
-    }
 
-    public BattleHud Hud
-    {
-        get { return hud; }
-    }
-
-
-    public Monster Monster { get; set; }
+    public bool IsPlayerUnit { get { return isPlayerUnit; } }
+    public BattleHud Hud { get { return hud; } }
+    public Monster monster { get; set; }    
 
     Image image;
     Vector3 originalPos;
@@ -33,25 +22,20 @@ public class BattleUnit : MonoBehaviour
     {
         image = GetComponent<Image>();
         originalPos = image.transform.localPosition;
+        originalColor = image.color;
     }
-    public void Setup(Monster monster)
-    {
+
+    public void Setup(Monster monster) 
+    {   
         monster = monster;
         if (isPlayerUnit)
             GetComponent<Image>().sprite = monster.Base.BackSprite;
         else
-            GetComponent<Image>().sprite = monster.Base.frontSprite;
-
+            GetComponent<Image>().sprite = monster.Base.frontSprite;    
+        
         hud.SetData(monster);
 
         image.color = originalColor;
     }
-    public void PlayEnterAnimations()
-    {
-        if (isPlayerUnit)
-            image.transform.localPosition = new Vector3(-500f, originalPos.y);
-        else
-            image.transform.localPosition = new Vector3(500f, originalPos.y);
-    }
-
 }
+
