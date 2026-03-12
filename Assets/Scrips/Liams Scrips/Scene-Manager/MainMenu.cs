0d020bb7ddcf76f3038f204ca46f736fa5e7d238
Nogard-Inmo <liam.nordlund09@gmail.com>
2026-03-12 21:15:55 +0100

    Added credits

--- Assets/Scrips/Liams Scrips/Scene-Manager/MainMenu.cs
+++ Assets/Scrips/Liams Scrips/Scene-Manager/MainMenu.cs
@@ -5,6 +5,12 @@ public class MainMenu : MonoBehaviour
 {
 
     public GameObject howToPlayMenu;
+    public GameObject creditsMenu;
+    public GameObject startButton;
+    public GameObject creditsButton;
+    public GameObject howToPlayButton;
+
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
@@ -12,12 +18,37 @@ public class MainMenu : MonoBehaviour
 
     public void HowToPLay()
     {
+        howToPlayButton.SetActive(false);
+        startButton.SetActive(false);
+        creditsButton.SetActive(false);
+
         howToPlayMenu.SetActive(true);
     }
 
     public void CloseHowToPlayMenu()
     {
+        howToPlayButton.SetActive(true);
+        startButton.SetActive(true);
+        creditsButton.SetActive(true);
+
         howToPlayMenu.SetActive(false);
+    }
+
+    public void OpenCredits()
+    {
+        howToPlayButton.SetActive(false);
+        startButton.SetActive(false);
+        creditsButton.SetActive(false);
+
+        creditsMenu.SetActive(true);
+    }
+    public void CloseCredits()
+    {
+        howToPlayButton.SetActive(true);
+        startButton.SetActive(true);
+        creditsButton.SetActive(true);
+
+        creditsMenu.SetActive(false);
     }
 }
 
